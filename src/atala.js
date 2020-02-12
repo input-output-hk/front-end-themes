8ab26936da44828122b8b101b8292f44c28c79d3
Tomas Vrana <tomas@vrana.org>
2020-02-12 15:51:43 +0100

    default setup extended

--- src/atala.js
+++ src/atala.js
@@ -1,11 +1,55 @@
 import { extendDefaultTheme } from './defaultTheme'
 
 export default extendDefaultTheme({
-  primary: 'blue',
-  secondary: 'red',
+  colors: {
+    primary: '#E5493E',
+    secondary: '#40eaea',
+    tertiary: '#E5493E',
+    text: {
+      primary: '#222',
+      secondary: '#fff',
+      tertiary: '#40eaea',
+      inverted: '#fff'
+    },
+    background: {
+      default: '#fff',
+      primary: '#fff',
+      secondary: '#E5493E',
+      tertiary: '#011727'
+    }
+  },
   typography: {
+    googleFontsURL: 'https://fonts.googleapis.com/css?family=Poppins:300,600,700',
+    baseFontSize: 10,
+    lineHeight: 1.6,
     h1: {
-      fontWeight: 100
+      fontSize: 5,
+      fontWeight: 700
+    },
+    h2: {
+      fontSize: 2.8,
+      fontWeight: 600,
+      lineHeight: 1.3
+    },
+    h3: {
+      fontSize: 1.8,
+      fontWeight: 600
+    },
+    h4: {
+      fontSize: 1.4,
+      fontWeight: 600
+    },
+    h5: {
+      fontSize: 1.3,
+      fontWeight: 600
+    },
+    h6: {
+      fontSize: 1.2,
+      fontWeight: 700
+    },
+    body2: {
+      fontSize: 1.4,
+      lineHeight: 1.7
     }
   }
 })
--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -1,21 +1,125 @@
 const baseConfig = {
   colors: {
-    primary: '#000',
-    secondary: '#fff',
-    text: '#000',
-    secondaryText: '#333',
+    primary: '#ff0',
+    secondary: '#f00',
+    tertiary: '#555',
+    text: {
+      default: '#000',
+      primary: '#222',
+      secondary: '#555',
+      tertiary: '#aaa',
+      inverted: '#fff'
+    },
     background: {
       default: '#fff',
-      alternative: '#ff0'
+      primary: '#fff',
+      secondary: '#222',
+      tertiary: '#0ff'
+    },
+    action: {
+      active: '#fff',
+      activeBackground: '#f83633',
+      disabled: '#555',
+      disabledBackground: '#999',
+      hover: '#f0f',
+      hoverBackground: '#f83633',
+      selected: '#000',
+      selectedBackground: '#f83666'
     }
   },
   typography: {
     fontFamily: 'Poppins',
-    googleFontsURL: 'https://fonts.googleapis.com/css?family=Poppins:300,400,700',
+    googleFontsURL: 'https://fonts.googleapis.com/css?family=Poppins:300,400',
     baseFontSize: 10,
+    lineHeight: 1.6,
     h1: {
       fontSize: 4,
-      fontWeight: 400
+      fontWeight: 400,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    h2: {
+      fontSize: 3,
+      fontWeight: 400,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    h3: {
+      fontSize: 2.2,
+      fontWeight: 400,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    h4: {
+      fontSize: 2,
+      fontWeight: 400,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    h5: {
+      fontSize: 1.8,
+      fontWeight: 700,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    h6: {
+      fontSize: 1.6,
+      fontWeight: 700,
+      letterSpacing: 1,
+      lineHeight: 1.2,
+      textTransform: 'none'
+    },
+    body1: {
+      fontSize: 1,
+      fontWeight: 200,
+      letterSpacing: 0,
+      lineHeight: 1.7
+    },
+    body2: {
+      fontSize: 0.8,
+      fontWeight: 300,
+      letterSpacing: 0,
+      lineHeight: 1.8
+    },
+    subtitle1: {
+      fontSize: 1,
+      fontWeight: 300,
+      letterSpacing: 1,
+      lineHeight: 1.7,
+      textTransform: 'none'
+    },
+    subtitle2: {
+      fontSize: 0.8,
+      fontWeight: 300,
+      letterSpacing: 1,
+      lineHeight: 1.8,
+      textTransform: 'none'
+    },
+    button: {
+      fontSize: 1,
+      fontWeight: 300,
+      letterSpacing: 1,
+      lineHeight: 1.7,
+      textTransform: 'uppercase'
+    },
+    caption: {
+      fontSize: 0.8,
+      fontWeight: 300,
+      letterSpacing: 1,
+      lineHeight: 1.8,
+      textTransform: 'none'
+    },
+    small: {
+      fontSize: 0.6,
+      fontWeight: 400,
+      letterSpacing: 1,
+      lineHeight: 1.8,
+      textTransform: 'uppercase'
     }
   }
 }
