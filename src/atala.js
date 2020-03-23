62596513b24ec354b2a6a80edd0f00e20e4665c1
Tomas Vrana <tomas@vrana.org>
2020-03-23 16:51:23 +0100

    atala theme font fixes

--- src/atala.js
+++ src/atala.js
@@ -27,29 +27,41 @@ export default extendDefaultTheme({
     }
   },
   typography: {
+    fontFamily: 'Poppins, sans-serif',
+    googleFontsURL: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700',
+    baseFontSize: 14,
+    lineHeight: 1.6,
+    letterSpacing: 0.5,
+    fontWeight: 400,
+    body: {
+      fontSize: 1.3,
+      fontWeight: 300,
+      letterSpacing: 1,
+      lineHeight: 1.7
+    },
     h1: {
-      fontSize: 5,
+      fontSize: 4,
       fontWeight: 700
     },
     h2: {
-      fontSize: 2.8,
+      fontSize: 2,
       fontWeight: 600,
       lineHeight: 1.3
     },
     h3: {
-      fontSize: 1.8,
-      fontWeight: 600
-    },
-    h4: {
-      fontSize: 1.4,
-      fontWeight: 600
-    },
-    h5: {
       fontSize: 1.3,
       fontWeight: 600
     },
-    h6: {
+    h4: {
       fontSize: 1.2,
+      fontWeight: 600
+    },
+    h5: {
+      fontSize: 1.1,
+      fontWeight: 600
+    },
+    h6: {
+      fontSize: 1,
       fontWeight: 700
     }
   }
